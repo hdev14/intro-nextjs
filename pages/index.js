8589088d4583546f8f1101068105411e91607ccf
Hermerson Araújo <ne.hermerson@gmail.com>
2021-03-23 09:46:09 -0300

    improve home page with posts data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+import utilsStyles from '../styles/utils.module.css';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -19,6 +21,31 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>)
         </p>
       </section>
+
+      <section className={`${utilsStyles.headingMd} ${utilsStyles.padding1px}`}>
+        <h2 className={utilsStyles.headingLg}>Blog</h2>
+        <ul className={utilsStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilsStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
